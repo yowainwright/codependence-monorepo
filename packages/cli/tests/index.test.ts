2fcb34b207a8e78eb99c898a1b27670afb0ce21c
Jeff Wainwright <yowainwright@gmail.com>
2024-04-21 22:15:43 -0700

    chore: updates eslint

--- packages/cli/tests/index.test.ts
+++ packages/cli/tests/index.test.ts
@@ -2,7 +2,7 @@ import { test, expect } from "vitest";
 import { action } from "../src/index";
 
 test("action", () => {
-  const result: any = action({ isTesting: true });
+  const result = action({ isTesting: true });
   console.log({ result });
   expect(result?.codependence).toBeDefined();
 });
